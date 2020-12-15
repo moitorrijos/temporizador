b6346a67135f143fb58bb9eaf7ea3d3135b05b3f
moitorrijos <juanmtorrijos@gmail.com>
2020-12-15 18:47:04 -0500

    Actualizar fecha al 21 de diciembre.

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 14, 2020 19:00:00").getTime();
+var countDownDate = new Date("Dec 21, 2020 14:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
