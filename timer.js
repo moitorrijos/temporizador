454823c01ba7b32251ca6c14e5eb072e7e043539
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2021-05-11 12:25:13 -0500

    Update count down date.

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 21, 2020 14:00:00").getTime();
+var countDownDate = new Date("May 13, 2021 19:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
